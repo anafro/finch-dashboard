57376beab72fe86036fa06fc42dc3c9e72984bd2
Anatoly Frolov <contact@anafro.ru>
2023-05-08 07:17:22 +1000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module ru.anafro.finch.finchrobotproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+                requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+                requires eu.hansolo.tilesfx;
+        
+    opens ru.anafro.finch.finchrobotproject to javafx.fxml;
+    exports ru.anafro.finch.finchrobotproject;
+}
