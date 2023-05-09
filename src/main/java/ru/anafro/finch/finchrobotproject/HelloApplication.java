2b9b1b3229c1d21df3757dcb1ff7123345603705
Anatoly Frolov <contact@anafro.ru>
2023-05-09 15:26:28 +1000

    Disable split pane division changing

--- src/main/java/ru/anafro/finch/finchrobotproject/HelloApplication.java
+++ src/main/java/ru/anafro/finch/finchrobotproject/HelloApplication.java
@@ -3,6 +3,8 @@ package ru.anafro.finch.finchrobotproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +14,16 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
         stage.setTitle("Finch Robot - Control panel");
         stage.setScene(scene);
+
+        // Disabling the split pane division changing:
+        SplitPane splitPane = (SplitPane) scene.lookup("#split-pane");
+        AnchorPane topPane = (AnchorPane) splitPane.getItems().get(0);
+        topPane.maxHeightProperty().bind(splitPane.heightProperty().multiply(0.085));
+        topPane.minHeightProperty().bind(splitPane.heightProperty().multiply(0.085));
+
         stage.show();
     }
 
