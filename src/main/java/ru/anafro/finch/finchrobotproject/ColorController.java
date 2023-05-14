276d7afe3cff4dfd4d2168237f977c050bab1448
vikesaki <faishalkemal68@gmail.com>
2023-05-14 19:38:51 +1000

    added working Color Picker

---
+++ src/main/java/ru/anafro/finch/finchrobotproject/ColorController.java
@@ -0,0 +1,33 @@
+package ru.anafro.finch.finchrobotproject;
+
+
+import javafx.scene.paint.Color;
+
+public class ColorController {
+    private static final Finch finch = HelloApplication.finch;
+
+    public void SetFrontLight (Color currentColor) {
+        //the color is from the ui is between 0 and 1
+        double Red = Math.round((100)*currentColor.getRed());
+        double Green = Math.round((100)*currentColor.getGreen());
+        double Blue = Math.round((100)*currentColor.getBlue());
+        finch.setBeak((int)Red,(int)Green,(int)Blue);
+        /*color test stuff
+        System.out.println("R : " + LeftCurrentColor.getRed() + " G : " + LeftCurrentColor.getGreen() + " B : " + LeftCurrentColor.getBlue());
+        System.out.println("R : " + (int)Red + " G : " + (int)Green + " B : " + (int)Blue + "\n");
+        */
+    }
+
+    public void SetBackLight (Color currentColor) {
+        //the color is from the ui is between 0 and 1
+        double Red = Math.round((100)*currentColor.getRed());
+        double Green = Math.round((100)*currentColor.getGreen());
+        double Blue = Math.round((100)*currentColor.getBlue());
+        finch.setTail("all", (int)Red,(int)Green,(int)Blue);
+    }
+
+    public void TurnOffLight () {
+        finch.setBeak(0, 0, 0);
+        finch.setTail("all",0, 0, 0);
+    }
+}
--- src/main/java/ru/anafro/finch/finchrobotproject/HelloController.java
+++ src/main/java/ru/anafro/finch/finchrobotproject/HelloController.java
@@ -1,9 +1,13 @@
 package ru.anafro.finch.finchrobotproject;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +16,7 @@ public class HelloController implements Initializable {
 
     private static final Finch finch = HelloApplication.finch;
 
+    public ColorController colorController = new ColorController();
     @FXML
     private Label labelLightLeft;
     @FXML
@@ -23,10 +28,37 @@ public class HelloController implements Initializable {
     @FXML
     private Label labelSpeed;
 
+    @FXML
+    Circle LeftCircleColor = new Circle();
+    @FXML
+    Circle RightCircleColor = new Circle();
+    @FXML
+    Circle TopCircleColor1 = new Circle();
+    @FXML
+    Circle TopCircleColor2 = new Circle();
+    @FXML
+    Circle TopCircleColor3 = new Circle();
+    @FXML
+    Circle TopCircleColor4 = new Circle();
+    @FXML
+    ColorPicker LeftColorPicker = new ColorPicker();
+    @FXML
+    ColorPicker RightColorPicker = new ColorPicker();
+    @FXML
+    ColorPicker TopColorPicker = new ColorPicker();
+    @FXML
+    ColorPicker AllColorPicker = new ColorPicker();
+    @FXML
+    Button TurnOffButton = new Button();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Thread thread = new Thread(this::UpdateStats);
+        Thread thread2 = new Thread(this::ColorPickerStuff);
+        Thread thread3 = new Thread(this::TurnOffLight);
         thread.start();
+        thread2.start();
+        thread3.start();
     }
 
     public void UpdateStats() {
@@ -38,21 +70,88 @@ public class HelloController implements Initializable {
             }
 
             labelLightLeft.setText("Light Left: " + finch.getLight("Left") + "%");
-            labelLightRight.setText("Right: " + finch.getLight("Right") + "%");
+            labelLightRight.setText("Light Right: " + finch.getLight("Right") + "%");
             labelCompass.setText("Compass: " + finch.getCompass() + " degrees");
-            labelDistance.setText("Distance: " + finch.getDistance() + " cm");
-            labelSpeed.setText("Speed: " + FinchController.speed + " ups");
-            FinchController.CheckDistance();
-        });
+            labelDistance.setText("Distance to obstacle: " + finch.getDistance() + " cm");
 
-        Sleep(200);
-        UpdateStats();
+            double[] acceleration = finch.getAcceleration();
+            double fSpeed = Math.sqrt(Math.pow(acceleration[0], 2) + Math.pow(acceleration[1], 2) + Math.pow(acceleration[2], 2));
+            labelSpeed.setText("Speed: " + fSpeed + " value");
+
+            Sleep(200);
+            UpdateStats();
+        });
     }
+
+    /**
+     * this is just for taking the value of the colorpicker
+     * need to change the ui because basically one of the button
+     * is useless.
+     * theres only beak (front) light and tail (top) light
+     */
+    public void ColorPickerStuff () {
+        LeftColorPicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Color LeftCurrentColor = LeftColorPicker.getValue();
+                LeftCircleColor.setFill(LeftCurrentColor);
+                colorController.SetFrontLight(LeftCurrentColor);
+            }
+        });
+        RightColorPicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Color RightCurrentColor = RightColorPicker.getValue();
+                RightCircleColor.setFill(RightCurrentColor);
+                colorController.SetBackLight(RightCurrentColor);
+            }
+        });
+        TopColorPicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Color TopCurrentColor = TopColorPicker.getValue();
+                TopCircleColor1.setFill(TopCurrentColor);
+                TopCircleColor2.setFill(TopCurrentColor);
+                TopCircleColor3.setFill(TopCurrentColor);
+                TopCircleColor4.setFill(TopCurrentColor);
+            }
+        });
+        AllColorPicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Color CurrentColor = AllColorPicker.getValue();
+                RightCircleColor.setFill(CurrentColor);
+                LeftCircleColor.setFill(CurrentColor);
+                TopCircleColor1.setFill(CurrentColor);
+                TopCircleColor2.setFill(CurrentColor);
+                TopCircleColor3.setFill(CurrentColor);
+                TopCircleColor4.setFill(CurrentColor);
+                RightColorPicker.setValue(CurrentColor);
+                LeftColorPicker.setValue(CurrentColor);
+                colorController.SetBackLight(CurrentColor);
+                colorController.SetFrontLight(CurrentColor);
+            }
+        });
+    }
+
+    /**
+     * turn off light button
+     */
+    public void TurnOffLight () {
+        TurnOffButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                colorController.TurnOffLight();
+            }
+        });
+    }
+
+
 
     /**
      * @param time time for sleep in milliseconds
      */
-    public static void Sleep(int time)
+    static void Sleep(int time)
     {
         try {
             Thread.sleep(time);
